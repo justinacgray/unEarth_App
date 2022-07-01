30aab2dc1294e0e0063e15047dbaa6ccfa307592
Justina Gray <justinacgray@gmail.com>
2022-07-01 02:07:46 -0400

    test

--- flask-server/app/controllers/search_controller.py
+++ flask-server/app/controllers/search_controller.py
@@ -1,5 +1,4 @@
 # from flask import Blueprint, render_template, redirect, request
-# from flask_app import app
-# from flask_app.models.user_model import Search
+# from app import app
+# from app.models.user_model import Search
 
-# search_controller = Blueprint('search_controller', __name__)
